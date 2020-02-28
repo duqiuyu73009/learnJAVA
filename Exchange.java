0321abe53c2230f83cc255d5001b8266e7935975
prince <1836596492@qq.com>
2020-02-28 13:03:37 +0800

    交换两个数字

---
+++ Exchange.java
@@ -0,0 +1,16 @@
+/*
+  交换两个数a和b
+ */
+
+
+public class Exchange{
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int t=a;		
+		a=b;
+		b=t;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+	}
+}
