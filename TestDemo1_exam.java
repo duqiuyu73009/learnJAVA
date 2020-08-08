fb8ca443f72af2e83b381a9e5415ced31f4ff76b
duqiuyu73009 <1498025553@qq.com>
2020-08-08 22:47:05 +0800

    ArrayList

---
+++ TestDemo1_exam.java
@@ -0,0 +1,21 @@
+package Exam;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestDemo1_exam {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k){
+        ArrayList<Integer> list = new ArrayList<>();
+//        ArrayList<Integer> result  = new ArrayList<>();
+//
+//        if (input == null || k > input.length || k <= 0){
+//            return null;
+//        }
+//        for (int i = 0; i < input.length; i++){
+//            list.add(input[i]);
+//        }
+//        Collections.sort(list);
+//        list = (ArrayList<Integer>) list.subList(0,k);
+      return list;
+    }
+}
