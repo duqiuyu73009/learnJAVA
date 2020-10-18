3121f032922131b44be7fa4f8073dc77287c9ae5
duqiuyu73009 <1498025553@qq.com>
2020-10-18 16:11:06 +0800

    单链表的main方法实现

---
+++ TestDemo1018_2.java
@@ -0,0 +1,14 @@
+package day1018;
+
+public class TestDemo1018_2 {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addFirst(10);
+        myLinkedList.addFirst(11);
+        myLinkedList.addFirst(12);
+        myLinkedList.addFirst(13);
+        myLinkedList.addFirst(14);
+
+        myLinkedList.display();
+    }
+}
