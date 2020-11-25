52fa5780c7c13d6f66f9353bf48d530802cddab5
duqiuyu73009 <1498025553@qq.com>
2020-11-25 23:20:57 +0800

    多态练习

---
+++ TestDemo1111_1.java
@@ -0,0 +1,54 @@
+package Test1;
+
+class Animal {
+    protected String name;
+    public Animal(String name) {
+        this.name = name;
+    }
+}
+interface IFlying {
+    void fly();
+}
+interface IRunning {
+    void run();
+}
+interface ISwimming {
+    void swim();
+}
+
+class Cat extends Animal implements IRunning {
+    public Cat(String name) {
+        super(name);
+    }
+    @Override
+    public void run() {
+        System.out.println(this.name + "正在用四条腿跑");
+    }
+}
+
+class Frog extends Animal implements IRunning, ISwimming {
+    public Frog(String name) {
+        super(name);
+    }
+    @Override
+    public void run() {
+        System.out.println(this.name + "正在往前跳");
+    }
+    @Override
+    public void swim() {
+        System.out.println(this.name + "正在蹬腿游泳");
+    }
+}
+
+
+
+public class TestDemo1111_1 {
+    public static void walk(IRunning running) {
+        System.out.println("我带着伙伴去散步");
+        running.run();
+    }
+    public static void main(String[] args) {
+        Cat cat = new Cat("huahua");
+        walk(cat);
+    }
+}
