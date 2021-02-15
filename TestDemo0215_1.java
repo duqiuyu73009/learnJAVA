02d7c0e4ed436038084a954cd783dd83e9a7bfef
duqiuyu73009 <1498025553@qq.com>
2021-02-15 22:58:11 +0800

    给一个整数数组 nums ，找出一个连续的子数列，如果对这个数列进行升序排列，那么整个数组就会变成升序

---
+++ TestDemo0215_1.java
@@ -0,0 +1,83 @@
+package day21_2_15;
+
+//给你一个整数数组 nums ，你需要找出一个 连续子数组 ，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+//
+//请你找出符合题意的 最短 子数组，并输出它的长度。
+//
+//来源：力扣（LeetCode）
+
+//示例 1：
+//
+//输入：nums = [2,6,4,8,10,9,15]
+//输出：5
+//解释：你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+import java.util.Arrays;
+
+public class TestDemo0215_1 {
+    //方法一
+    public int findUnsortedSubarray1(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j <= nums.length; j++) {
+                int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, prev = Integer.MIN_VALUE;
+                for (int k = i; k < j; k++) {
+                    min = Math.min(min, nums[k]);
+                    max = Math.max(max, nums[k]);
+                }
+                if ((i > 0 && nums[i - 1] > min) || (j < nums.length && nums[j] < max))
+                    continue;
+                int k = 0;
+                while (k < i && prev <= nums[k]) {
+                    prev = nums[k];
+                    k++;
+                }
+                if (k != i)
+                    continue;
+                k = j;
+                while (k < nums.length && prev <= nums[k]) {
+                    prev = nums[k];
+                    k++;
+                }
+                if (k == nums.length) {
+                    res = Math.min(res, j - i);
+
+                }
+            }
+        }
+        return res;
+    }
+
+    //方法二
+    public int findUnsortedSubarray2(int[] nums) {
+        int l = nums.length, r = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                    r = Math.max(r, j);
+                    l = Math.min(l, i);
+                }
+            }
+        }
+        return r - l < 0 ? 0 : r - l + 1;
+    }
+
+    //方法三
+    public int findUnsortedSubarray3(int[] nums) {
+        int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length, end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+    }
+
+}
