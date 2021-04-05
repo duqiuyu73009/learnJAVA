93cf01785ea3ccdbea475eebb5b294b553b33308
duqiuyu73009 <1498025553@qq.com>
2021-04-05 23:09:00 +0800

    ArticleServlet.java

---
+++ ArticleServlet.java
@@ -0,0 +1,69 @@
+package api;
+
+import model.Article;
+import model.ArticleDao;
+import model.User;
+import model.UserDao;
+import view.HtmlGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+public class ArticleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        //1.验证用户是否已经登陆了，如果尚未登陆提示用户登录
+        HttpSession httpSession = req.getSession(false);
+        if (httpSession == null){
+            //当前是未登录状态
+            String html = HtmlGenerator.getMessagePage("请先登录","login.html");
+            resp.getWriter().write(html);
+            return;
+        }
+        User user = (User)httpSession.getAttribute("user");
+        //2.判断请求中是否存在articleId参数
+        String articleIdStr = req.getParameter("articleId");
+        if (articleIdStr == null){
+            //a）没有这个参数就去执行获取文章列表的操作
+            getAllArticle(user, resp);
+        }else {
+            //b）有这个参数就去执行获取文章详情的操作
+            getOneArticle(Integer.parseInt(articleIdStr),user,resp);
+        }
+
+    }
+
+    private void getOneArticle(int articleId, User user, HttpServletResponse resp) throws IOException {
+        //1.查找数据库
+        ArticleDao articleDao = new ArticleDao();
+        Article article = articleDao.selectById(articleId);
+        if (article == null){
+            //文章未找到
+            String html = HtmlGenerator.getMessagePage("文章不存在","article");
+            resp.getWriter().write(html);
+            return;
+        }
+        //根据作者ID。找到作者信息，进一步得到作者姓名
+        UserDao userDao = new UserDao();
+        User author = userDao.selectById(article.getUserId());
+        //2.构造页面
+        String html = HtmlGenerator.getArticleDetailPage(article,user,author);
+        resp.getWriter().write(html);
+    }
+
+
+    private void getAllArticle(User user, HttpServletResponse resp) throws IOException {
+        //1.查找数据库
+        ArticleDao articleDao = new ArticleDao();
+        List<Article> articles = articleDao.selectAll();
+        //2.构造页面
+        String html = HtmlGenerator.getArticleListPage(articles, user);
+        resp.getWriter().write(html);
+    }
+}
