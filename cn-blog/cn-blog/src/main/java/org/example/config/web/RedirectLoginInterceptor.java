040533d5d9961757108cde378bdd17049612c035
duqiuyu73009 <1498025553@qq.com>
2021-04-06 22:34:31 +0800

    cn-blog

---
+++ cn-blog/cn-blog/src/main/java/org/example/config/web/RedirectLoginInterceptor.java
@@ -0,0 +1,25 @@
+package org.example.config.web;
+
+import org.example.model.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class RedirectLoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取session，如果获取不到，返回null
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            User user = (User) session.getAttribute("user");
+            if(user != null){
+                return true;
+            }
+        }
+        //前端敏感资源，没有登录时，直接重定向到登录页面即可
+        response.sendRedirect("/views/login.html");
+        return false;
+    }
+}
